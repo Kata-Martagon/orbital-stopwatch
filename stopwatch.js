bc5c2525332fa2db68337e66cc046d461b1c97ea
Matt <matthewsharp123@gmail.com>
2016-06-15 12:39:37 +0100

    new tests - we are far too methodical

--- stopwatch.js
+++ stopwatch.js
@@ -4,18 +4,22 @@ var increment = 0;
 
 function run() {
   increment++;
+  console.log(increment)
   return increment;
 };
 
 function start() {
-  return run();
+  var test = setInterval(run,10);
 };
 
 function reset(){
   increment = 0;
-  // return increment;
 }
 
 document.getElementById("start").addEventListener("click", function() {
   start();
+});
+
+document.getElementById("reset").addEventListener("click", function() {
+  reset();
 });
--- test.js
+++ test.js
@@ -12,7 +12,7 @@ test( 'if the function has cycled once, it will return 1 the next time', functio
 });
 
 test( 'start calls the run function', function(assert) {
-  var actual = start();
+  var actual = increment;
   var expected = run() - 1;
   assert.equal(actual, expected, 'start called run function');
 });
@@ -27,6 +27,32 @@ test( 'Reset resets run to 0', function(assert) {
 test( 'start button called start with a click', function(assert) {
   document.getElementById("start").click();
   var actual = increment;
-  var expected = start() - 1;
-  assert.equal(actual, expected, '');
+  var expected = run() - 1;
+  assert.equal(actual, expected, 'start button works');
+});
+
+test( 'reset button called reset with a click', function(assert) {
+  document.getElementById("reset").click();
+  var actual = increment;
+  var expected = 0;
+  assert.equal(actual, expected, 'reset button works');
+});
+
+
+
+test( 'function setTimeout is called after other tests', function(assert) {
+  actual = increment === 0;
+
+  document.getElementById("start").click();
+  var timeOut = setTimeout(testCont, 2000);
+
+  function testCont() {
+    test( 'Start runs continously', function(assert) {
+      console.log(increment);
+      var actual = increment > 5;
+      assert.ok(actual, 'start runs');
+    });
+  };
+
+  assert.ok(actual, 'called after other tests');
 });
